351b4d5db3d09432292f041442d0b1c1ca4122e8
Eric Pedley <ericpedley@gmail.com>
2021-01-09 18:57:49 -0800

    fixed egregious idiot bugs

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 from keyinput import pressKey, releaseKey
 import random
-import math
+from intersection_math import getIntersection
 # kanye head is 56x76
 rdown = False
 ldown = False
@@ -12,47 +12,8 @@ print(origin)
 prevpos = None
 
 
-# returns the x coords of the intersection with a circle
-def getLineCircleIntersection(delta, center):
-    a = delta[1]/delta[0]  # slope of line
-    b = center[1]-a*center[0]  # y intercept of line
-    c = 465//2  # canvas width/2
-    d = 466//2  # canvas height/2
-    e = 39  # circle radius
-    negb = -2*(a*(b-d)-c)
-    bsq = negb**2
-    neg4ac = -4*(a*a+1)*(b**2-2*b*d+d**2+c**2-e**2)
-    if bsq > neg4ac:  # two solutions
-        return ((negb+math.sqrt(bsq+neg4ac))/(2*(a**2+1)), (negb-math.sqrt(bsq+neg4ac))/(2*(a**2+1)))
-    elif bsq == neg4ac:  # one solutions
-        return negb/(2*(a**2+1))
-    else:  # no intersection
-        return None
 
-
-def getIntersection(delta, center):
-    for i in range(10):  # maximum iterations is 10 before it gives up trying to find an intersect
-        intersections = getLineCircleIntersection(delta, center)
-        # the following conditions checks whether there are intersections and whether one of them is
-        if intersections != None and (intersections[0][0] > center[0]) == (delta[0] > 0) or (intersections[1][0] > center[0]) == (delta[0] > 0):
-            return intersections
-        else:  # there are no intersections and we need to find the equation for the reflected line
-            slope = delta[1]/delta[0]
-            yint = 233  # the y coordinate at whichever side edge of the map, except this starts at halfway because it needs to be assigned here but idk to what, and 233 is between both
-            if delta[0] >= 0:  # kanye is moving to the right
-                # the y coordinate at the right edge of the map, with the right edge coordinate hard coded in
-                yint = slope*(465-center[0])+center[1]
-            else:  # kanye is moving to the left
-                yint = slope*-center[0]+center[1]
-            if yint > 466:  # intersection with top, which in the coordinate system is zero
-                delta = (delta[0], -delta[1])
-                center = (center[0]-center[1]/slope, 0)
-            elif yint < 0:
-                delta = (delta[0], -delta[1])
-                center = (center[0]+(466-center[1])/slope, 0)
-            else:  # bouncing off a side wall
-                delta = (-delta[0], delta[1])
-                center = (0 if delta[0] < 0 else 465, yint)
+            
 
 
 while(True):
@@ -68,17 +29,19 @@ while(True):
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     top_left = min_loc
     bottom_right = (top_left[0] + w, top_left[1] + h)
-    img = cv2.rectangle(img, top_left, bottom_right, 255, 2)
+    #img = cv2.rectangle(img, top_left, bottom_right, 255, 2) draws blue rect around kanye head
 
     center = (top_left[0]+w//2, top_left[1]+h//2)
-    if type(prevpos) != type(None):
+    if prevpos != None:
         delta = (center[0]-prevpos[0], center[1]-prevpos[1])
-        solution = None
-        while(solution == None):
-            intersection = getIntersection(
-                (center[0]-origin.left, center[1]-origin.top), delta)
-        print(delta)
-        img = cv2.line(img, center, prevpos, color=(0, 0, 255), thickness=2)
+        intersection = getIntersection(center=(center[0]-origin.left, center[1]-origin.top), delta=delta)
+        print(f"intersection: {intersection}")
+        if intersection!=None:
+            img = cv2.circle(img, (origin.left+int(intersection[0][0]),origin.top+int(intersection[0][1])), 10, color=(0, 255, 0), thickness=-1)
+            
+
+        #print(delta)
+        #img = cv2.line(img, center, prevpos, color=(0, 0, 255), thickness=2) draws line in direction of travel
     prevpos = center
     cv2.imshow("img", cv2.resize(img, (800, 450)))
    # print(cv2.matchTemplate(img,cv2.imread("kanye2.png"),0))
